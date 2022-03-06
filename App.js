33b1b6743f6e522fab907dffbc1c91f862414be9
felzek <yukunpengba@gmail.com>
2022-03-06 16:46:10 -0600

    add ui kitten

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+
+const HomeScreen = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category='h1'>HOME</Text>
+  </Layout>
+);
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up dApp.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApplicationProvider {...eva} theme={eva.light}>
+    <HomeScreen />
+  </ApplicationProvider>
   );
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
