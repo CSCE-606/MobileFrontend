51898efb45eddc0d4f85b9b25b6e6c8fd37f6758
wfy90320 <wfy980320@gmail.com>
2022-03-08 14:45:39 -0600

    welcomepage

--- App.js
+++ App.js
@@ -1,33 +1,6 @@
 import React from 'react';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Text } from '@ui-kitten/components';
-import { StyleSheet } from 'react-native';
-import { Avatar, Layout } from '@ui-kitten/components';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
-
-
-
-const HomeScreen = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Avatar style={styles.avatar} size='giant' source={require('./assets/dili.png')}/>
-    <Text category='h1'>Reba is missing you</Text>
-  </Layout>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    flexWrap: 'wrap',
-    padding: 8,
-  },
-  avatar: {
-    margin: 8,width:300, height:300
-  },
-});
-
-export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <HomeScreen />
-  </ApplicationProvider>
-);
+export default function App(){
+  return <WelcomeScreen/>;
+}
