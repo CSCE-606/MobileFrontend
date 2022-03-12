17f12fef1550ae7e291e584e31f6954c1a9ccc23
Chao Li <chaoli@client-10-228-22-244.tamulink.tamu.edu>
2022-03-11 22:12:18 -0600

    update

--- app/screens/friendlist.js
+++ app/screens/friendlist.js
@@ -1,109 +1,89 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, Button } from 'react-native';
+import React from "react";
+import { View, Text, Image, StyleSheet, Button } from "react-native";
 
 import AppText from "../components/AppText";
-import AppButton from '../components/AppButton';
-import ListItem from '../components/ListItem';
+import AppButton from "../components/AppButton";
+import ListItem from "../components/ListItem";
 
 export function FriendList() {
-    return(
-        <View style={styles.container}>
-            <View style={styles.listContainer} >
-                <View style={styles.listElement}>
-                    <ListItem 
-                        title = "Friend4"
-                        image = {require("../assets/fox.png")}
-                    />
-                </View>
+  const users = [
+    {
+      username: "liboyu"
+    },
+    {
+      username: "wuyue"
+    },
+    {
+      username: "wangfengyi"
+    },
+    {
+      username: "kai"
+    }
+  ];
+  return (
+    <View style={styles.container}>
+      <View style={styles.listContainer}>
+        {users.map((user, index) => (
+          <View style={styles.listElement}>
+            <ListItem
+              title={user.username}
+              image={require("../assets/fox.png")}
+            />
+          </View>
+        ))}
+      </View>
 
-                <View style={styles.listElement}>
-                    <ListItem 
-                        title = "Friend4"
-                        image = {require("../assets/fox.png")}
-                    />
-                </View>
-
-                <View style={styles.listElement}>
-                    <ListItem 
-                        title = "Friend4"
-                        image = {require("../assets/fox.png")}
-                    />
-                </View>
-                <View style={styles.listElement}>
-                    <ListItem 
-                        title = "Friend4"
-                        image = {require("../assets/fox.png")}
-                    />
-                </View>
-
-                <View style={styles.listElement}>
-                    <ListItem 
-                        title = "Mosh"
-                        image = {require("../assets/fox.png")}
-                    />
-                </View>
-
-            </View>
-
-            
-
-            <View style={styles.buttonContainer}>
-                <AppButton title="Button1"  />
-                <AppButton title="Button2"  />
-                <AppButton title="Button3"  />
-                <AppButton title="Button4" />
-            </View>
-            
-        </View>
-    )
+      <View style={styles.buttonContainer}>
+        <AppButton title="Button1" />
+        <AppButton title="Button2" />
+        <AppButton title="Button3" />
+        <AppButton title="Button4" />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    buttonContainer: {
-        flexDirection: "row",
-        
-        top: 150,
-        flex: 0.05,
-        borderRadius: 10,
-        shadowColor: "grey",
-        shadowOffset: {width: 10, height: 10},
-        shadowOpacity: 1,
-    },
+  buttonContainer: {
+    flexDirection: "row",
 
-    container: {
-        flex: 1,
-        backgroundColor: "#f9e955",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+    top: 150,
+    flex: 0.05,
+    borderRadius: 10,
+    shadowColor: "grey",
+    shadowOffset: { width: 10, height: 10 },
+    shadowOpacity: 1
+  },
 
-    listElement: {
-        flexDirection: "row",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#f9e955",
+    alignItems: "center",
+    justifyContent: "center"
+  },
 
-    listContainer: {
-        flex: 0.5,
-        backgroundColor: "#f9e955",
-        right: 100,
-        justifyContent: "space-evenly",
-    },
+  listElement: {
+    flexDirection: "row"
+  },
 
-    image: {
-        width: 70,
-        height: 70,
-        borderRadius: 35,
+  listContainer: {
+    flex: 0.5,
+    backgroundColor: "#f9e955",
+    right: 100,
+    justifyContent: "space-evenly"
+  },
 
-    },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35
+  },
 
-    textElement: {
-       
-    },
+  textElement: {},
 
-    userContainer: {
-        marginVertical: 40,
-
-    }
-})
-
+  userContainer: {
+    marginVertical: 40
+  }
+});
 
 export default FriendList;
