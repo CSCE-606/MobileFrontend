b5baae03567e11452ef43db6612f90c14ff9006d
wfy90320 <wfy980320@gmail.com>
2022-03-25 17:50:10 -0500

    auth

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './app/navigation/AuthNavigator';
 import route from './app/navigation/routes';
-
 export default function App(){
   return(
     <NavigationContainer>
