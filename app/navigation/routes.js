08be7a1648db35a12c924098d1ba5d9ef2b55795
liboyu1999 <90221816+liboyu1999@users.noreply.github.com>
2022-04-02 15:41:31 -0500

    Delete app/navigation directory

--- app/navigation/routes.js
+++
@@ -1,9 +0,0 @@
-export default Object.freeze({
-    LISTING_DETAILS: "ListingDetails",
-    LISTING_EDIT: "ListingEdit",
-    LOGIN: "Login",
-    MESSAGES: "Messages",
-    REGISTER: "Register",
-    WELCOME : "Welcome",
-    FRIEND: "Freind",
-  });
