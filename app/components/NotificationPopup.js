1b57980f1897b68ad29f38b485f4376ea55c3f7d
zdong2080 <85309696+zdong2080@users.noreply.github.com>
2022-04-13 22:05:44 -0500

    notification component

---
+++ app/components/NotificationPopup.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import { Tip, showTip, closeTip } from "react-native-tip";
+import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import TipProvider from "react-native-tip";
+import { NotificationComponent} from './NotificationComponent';
+
+function NotificationPopup () {
+
+    const [_showTip, setShowTip] = useState(false)
+    const [friendName, setFriendName] = useState("User")
+
+ 
+    return(
+        <View>
+            <Tip
+                id='notification'
+                title={friendName+" send you a friend request"}
+                body={ <NotificationComponent /> }
+                showItemPulseAnimation
+                pulseColor='#ff8080'
+            >
+        
+                <TouchableOpacity
+                    
+                    onPress={() => {
+                        _showTip && showTip('notification')
+                        setShowTip(true)
+                    }}
+                    style={{ padding: 10, borderRadius: 50 }}
+                >
+                    <Icon name="heart" color='blue' size={35}/>
+                </TouchableOpacity>
+            </Tip>
+
+        <TipProvider
+                    overlayOpacity={0.5}
+                    titleStyle={{
+                        fontWeight: 'bold',
+                        fontSize: 18,
+                        marginBottom: 10
+                    }}
+                    bodyStyle={{
+                        fontSize: 16
+                    }}
+                    tipContainerStyle={{
+                        padding: 20,
+                        borderRadius: 20,
+                        maxWidth: 350,
+                        elevation: 5
+                    }}
+                    // darkMode={isDarkMode}
+                    prevNextTextStyle={{
+                    }}
+                    prevNextButtonStyle={{
+                    }}
+                />
+        
+        
+        
+        
+        
+        </View>
+
+
+    );
+}
+
+export default NotificationPopup
