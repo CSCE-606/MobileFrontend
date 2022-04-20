e5534e045a0c067711bad12dfef22a6cef1bda78
zdong2080 <85309696+zdong2080@users.noreply.github.com>
2022-04-20 16:47:30 -0500

    notification popup

--- app/components/NotificationPopup.js
+++ app/components/NotificationPopup.js
@@ -1,66 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { View, Text, StyleSheet, TouchableOpacity, Button, Modal, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { NotificationComponent} from './NotificationComponent';
 import styles from '../config/styles';
+import TipProvider from "react-native-tip";
+import { Tip, showTip } from "react-native-tip";
+import { setStatusBarNetworkActivityIndicatorVisible } from 'expo-status-bar';
+import { Directions } from 'react-native-gesture-handler';
 
 function NotificationPopup () {
 
-    // const [_showTip, setShowTip] = useState(false)
     const [friendName, setFriendName] = useState("User")
+    
+    const [show, setShow] = useState(false);
 
     return(
-        <View style={{left: 300, top: -300}}>
-            <Tip
-                id='notification'
-                title={friendName+" send you a friend request"}
-                body={ <NotificationComponent /> }
-                showItemPulseAnimation
-                pulseColor='#ff8080'
-            >
-        
-                <TouchableOpacity
-                    
-                    onPress={() => {
-                        _showTip && showTip('notification')
-                        setShowTip(true)
-                    }}
-                    style={{ padding: 10, borderRadius: 50 }}
-                >
-                    <Icon name="heart" color='blue' size={35} />
-                </TouchableOpacity>
-            </Tip> 
-
-        {/* <TipProvider
-                    overlayOpacity={0.5}
-                    titleStyle={{
-                        fontWeight: 'bold',
-                        fontSize: 18,
-                        marginBottom: 10
-                    }}
-                    bodyStyle={{
-                        fontSize: 16
-                    }}
-                    tipContainerStyle={{
-                        padding: 20,
-                        borderRadius: 20,
-                        maxWidth: 350,
-                        elevation: 5
-                    }}
-                    // darkMode={isDarkMode}
-                    prevNextTextStyle={{
-                    }}
-                    prevNextButtonStyle={{
-                    }}
-                /> */}
-        
-        
-        
-        
+        <View style={{top: -300, left: 300}}>
+            
+            <TouchableOpacity>
+                <Icon name='bell' size={20} color="red" onPress={() => {setShow(true)}} />
+            </TouchableOpacity>
+           <ScrollView>
+            <Modal transparent={true} visible={show}>
+                <View style={{backgroundColor: '#000000aa', flex: 1}}>
+                    <View style={{backgroundColor: '#ffffff', top: 100, left: 40, margin: 50, padding: 40, borderRadius: 10}}>
+                        <Icon name='close' color="red" onPress={() => { setShow(false) }} size={20} style={{top: -30, left: 200}} />
+                        <NotificationComponent />
+                        <NotificationComponent />
+                        <NotificationComponent />
+                        <NotificationComponent />
+                    </View>
+                </View>
+            </Modal>
+            </ScrollView>
         
         </View>
-
-
     );
 }
 
