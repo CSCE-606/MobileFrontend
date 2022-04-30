2c9dde8486c88b469c1257e4bbacdb73383ac038
liboyu <liboyu1999@tamu.edu>
2022-04-30 16:25:50 -0500

    update map

--- app/screens/Map.js
+++ app/screens/Map.js
@@ -2,44 +2,79 @@
 import React, { useState, useEffect } from 'react';
 import { View, Image,StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-import {getLocation} from "./Location";
+import * as Location from 'expo-location';
 
-console.log("teste", getLocation);
 
-const App = () => {
-  const [mapRegion, setmapRegion] = useState({
-    latitude: 30.616701789744717,
-    longitude:  -96.338583,
+
+const Map = () => {
+  // const [mapRegion, setmapRegion] = useState({
+    // latitude: 30.616701789744717,
+    // longitude:  -96.338583,
+    // latitudeDelta: 0.0922,
+    // longitudeDelta: 0.0421,
+  // });
+  
+
+  const [location, setLocation] = useState( {latitude: 15.616701789744717,
+    longitude:  -100.338583,
     latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  });
+    longitudeDelta: 0.0421,});
+  const [errorMsg, setErrorMsg] = useState(null);
+  console.log('tmd',location)
+
+
+  useEffect(() => {
+    (async () => {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let newlocation = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.Balanced,
+                enableHighAccuracy: true,
+                timeInterval: 5
+      });
+      const latitude = newlocation.coords.latitude;
+      const longitude = newlocation.coords.longitude;
+      const latitudeDelta = 0;
+
+
+      setLocation({latitude,
+        longitude,latitudeDelta});
+        
+   
+    })();
+  }, []);
   
-  console.log(`value of i is ${getLocation()}`);
-
-  
-
-
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } 
+  console.log('tnd',location)
   return (
     <View style={styles.container}>
       <MapView
         style={{ alignSelf: 'stretch', height: '100%' }}
-        region={mapRegion}
+        region={location}
       >
 
-        <Marker coordinate={mapRegion} title={'User Location'} >
-          < Image
-          
-           source={{uri:"https://firebasestorage.googleapis.com/v0/b/curastone-74faf.appspot.com/o/cd654793-481e-48cb-b176-9f78baae7e75.jpg?alt=media&token=91a9ced6-594a-4193-9582-59b32c0ac8f7"}}
-           style={{width: 50, height: 50, borderRadius : 1000, borderWidth: 3, borderColor: "blue"}}
-           
-          />
+        <Marker coordinate={location} title={'User Location'} >
+ 
         </Marker>
 
       </MapView>
     </View>
   );
 };
-export default App;
+
+
+
+export default Map;
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
